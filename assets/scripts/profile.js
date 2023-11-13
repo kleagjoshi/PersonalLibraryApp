c4616ca867baabf1c12a198d7f4cc1399152edb0
Sindi Ziu <sziu21@epoka.edu.al>
2023-11-13 01:01:22 +0100

    clickable divs, failed to add image alongside text

---
+++ assets/scripts/profile.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $(".read-flex").click(function () {
+        window.location = "read.html";
+    });
+    $(".to-be-read").click(function () {
+        window.location = "toBeRead.html";
+    });
+    
+});
