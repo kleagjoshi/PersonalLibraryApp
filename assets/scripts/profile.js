556bd8fc2db516736c16afc551e6c8b87d02e7cd
Sindi Ziu <sziu21@epoka.edu.al>
2023-11-13 17:01:40 +0100

    sorry sindi they said

---
+++ assets/scripts/profile.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $(".read").click(function () {
+        window.location = "read.html";
+    });
+    $(".to-be-read").click(function () {
+        window.location = "toBeRead.html";
+    });
+    
+});
