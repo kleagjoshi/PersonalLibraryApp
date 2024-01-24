eac4a9af66fb568cf6fa03352dc126dd71974b3b
Sindi Ziu <sziu21@epoka.edu.al>
2024-01-24 19:22:40 +0100

    edit&delete first

--- assets/scripts/library.js
+++ assets/scripts/library.js
@@ -32,6 +32,22 @@ $("#closeAddAuthorModalSpn").click(function(){
 });
 }
 
+function editFunction(event){
+    $("#editBookModal").show();
+
+$("#closeEditBookModalSpn").click(function(){
+    $("#editBookModal").hide();
+});
+}
+
+function deleteFunction(event){
+    $("#deleteBookModal").show();
+
+$("#closeDeleteBookModalSpn").click(function(){
+    $("#deleteBookModal").hide();
+});
+}
+
 //Add event to Add Book button
 document.addEventListener('DOMContentLoaded',(event)=>{
 
@@ -48,6 +64,21 @@ document.addEventListener('DOMContentLoaded',(event)=>{
     
 })
 
+//Add event to Edit Book button
+document.addEventListener('DOMContentLoaded',(event)=>{
+
+    const editButton = document.getElementById("editBookBtn");
+    addButton.addEventListener('click',editFunction)
+    
+})
+
+//Add event to Delete Book button
+document.addEventListener('DOMContentLoaded',(event)=>{
+
+    const editButton = document.getElementById("deleteBookBtn");
+    addButton.addEventListener('click',deleteFunction)
+    
+})
 
 
 var books = [];
@@ -87,7 +118,9 @@ function populateGrid(){
                     </div>
                     <div class="attr" id="">${book.title} <br> ${book.authorNames.join(', ')} <br>${book.genreTitle} </div>
                     <div>
-                         <button class ="glow-on-hover" type="button" name="readBtn" id="readBtn" data-read-id="${book.bookId}">Read Me</button>    
+                         <button class ="glow-on-hover" type="button" name="readBtn" id="readBtn" data-read-id="${book.bookId}">Read Me</button>
+                         <button class ="glow-on-hover" type="button" name="editBtn" id="editBtn" data-read-id="${book.bookId}">Edit</button> 
+                         <button class ="glow-on-hover" type="button" name="deleteBtn" id="deleteBtn" data-read-id="${book.bookId}">Delete</button>     
                     </div>
                 </div>`;
         
@@ -95,8 +128,6 @@ function populateGrid(){
         
     });
 }
-
-
 
 //this function makes the form function as it should
 
@@ -160,99 +191,171 @@ $(document).ready(function () {
 
 
 //add functionality to read me button
-const gridBody = document.getElementById("myGrid");
+var gridBody = document.getElementById("myGrid");
 
 $(gridBody).on('click', "#readBtn", function(){
-    const _bookId = $(this).data('read-id');
+    const bookId = $(this).data('read-id');
+    const book = books.find(n => n.id == bookId);
     
-   
-    //decode the token
-const token = localStorage.getItem('token');
 
-if (token) {
-  const decodedToken = parseJwt(token);
-  var _userId = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier']; 
-}
-// Function to decode a JWT token
-function parseJwt(token) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+    console.log($`Selected book = ${book}`);
 
-  return JSON.parse(jsonPayload);
-}
+    book.toBeRead=true;
 
-//add this record (user-book) to db
-
-var newUserBook = {
-    bookId: _bookId,
-    userId:_userId  
-};
-
-
-// api endpoint
-const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://localhost:44320/api/UserBooks/add-book-user?bookId=' + newUserBook.bookId + '&userId=' + newUserBook.userId,
-    method: 'POST',
-    headers: {
-        'content-type': 'application/json'
-    },
     
-};
+     // Update the orders in localStorage
+    localStorage.setItem('books', JSON.stringify(books));
 
-$.ajax(settings).done(function (response) {
-    alert('UserBook added to json file');
-});
+    $("#book-name").text(book.title);
 
-//$("#book-name").text(Book.title);
- $("#readModal").show();
-});
+    $("#readModal").show();
+})
 
 
 $("#closeReadSpn").click(function(){
     $("#readModal").hide();
 });
 
+// START OF EDIT
 
-// Search function
+//form functionality for EDIT
+function handleEditSubmit(_id, _title, _authors, _description, _genre, _image) {
+    // Convert author IDs to integers
+    const authorIds = _authors.map(authorId => parseInt(authorId, 10));
 
-// function search(){
+    // Create Object
+    var updatedBook = {
+        title: _title,
+        authorIds: authorIds, // Update to match the server's expected format
+        description: _description,
+        genreId: parseInt(_genre, 10), // Convert genre ID to integer
+        coverUrl: _image
+    };
 
-//     const test = document.getElementById("inputId").value;
-//     const book1=test.toLowerCase();
-
-//     var allBooks = JSON.parse(localStorage.getItem('books')) || [];
-    
-    
-//     const book = allBooks.find(n=> n.title.toLowerCase()==book1);
-//     if(book){
-//         alert("YAY!! \n The book ' "+book.title+" ' you are searching is in our library.");
-//     }
-//     else{
-//         alert("Error 404!!!\nHahaha Just kidding :) \nWe don't have this book.");
-//     }
-// }
-function searchBooks() {
-    var title = $('#searchTitle').val();
-
-    $.ajax({
-        url: 'https://localhost:44320/api/Books/search-book-by-title/${title}' ,
-        type: 'POST',
-        success: function (data) {
-            displaySearchResults(data);
-            alert("YAY!! \n The book ' "+book.title+" ' you are searching is in our library.");
+    //Request orders data from the api endpoint
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: `https://localhost:44320/api/Books/update-book/${_id}`,
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
         },
-        error: function (error) {
-            alert("Error 404!!!\nWe don't have this book :(.");
-            console.error('Error searching books:', error.responseText);
-        }
+        data: JSON.stringify(updatedBook)
+    };
+    console.log(updatedBook);
+
+    $.ajax(settings).done(function (response) {
+        alert('Book updated in json file');
     });
 }
 
+$(document).ready(function () {
+    $("#submitEditBtn").click(function () {
+        // Get values from form fields
+        var id = $("#bookId").val();
+        var title = $("#bookTitle").val();
+        
+        var authors = [];
+        $("#authorsDropdown input:checked").each(function () {
+            authors.push($(this).val());
+        });
+    
+        var description = $("#description").val();
+        var genre = $("#genre").val(); // Get the selected genre
+        var imageurl = $("#image").val();
+    
+        // Call the handleEditSubmit function with the form values
+        handleEditSubmit(id, title, authors, description, genre, imageurl);
+        $("#editBookModal").hide();
+        location.reload();
+    });
+})
+
+//add functionality to edit button
+$(gridBody).on('click', "#editBtn", function(){
+    const bookId = $(this).data('edit-id');
+    const book = books.find(n => n.id == bookId);
+    
+    populateEditForm(book);
+    $("#editBookModal").show();
+    
+    function populateEditForm(book){
+        $("#editBookTitle").val(book.title);
+        $("#editBookAuthor").val(book.authors);
+        $("#editBookDescription").val(book.description);
+        $("#editBookGenre").val(book.genre);
+        $("#editBookCoverUrl").val(book.coverUrl);
+    }
+})
+
+$("#closeEditModalSpn").click(function(){
+    $("#editBookModal").hide();
+});
+
+// END OF EDIT
+
+// START OF DELETE
+//add functionality to delete button
+$(gridBody).on('click', "#readBtn", function(){
+    function deleteFunction(event) {
+        const bookId = event.target.dataset.readId;
+        $("#deleteBookModal").show();
+      
+        // Add a click event listener to the confirm button
+        $("#confirmDeleteBtn").click(function () {
+          // Call the API to delete the book, like handlesubmit
+          const settings = {
+            async: true,
+            crossDomain: true,
+            url: `https://localhost:44320/api/Books/delete-a-book-by-id/${bookId}`,
+            method: 'DELETE',
+            headers: {
+              'content-type': 'application/json'
+            }
+          };
+      
+          $.ajax(settings).done(function (response) {
+            // Remove the book from the grid
+            $("#book-" + bookId).remove();
+            // Hide the confirmation modal
+            $("#deleteBookModal").hide();
+            alert('Book deleted!');
+          });
+        });
+      
+        // Add a click event listener to the cancel button
+        $("#cancelDeleteBtn").click(function () {
+          // Close the confirmation modal
+          $("#deleteBookModal").hide();
+        });
+      
+        $("#closeDeleteBookModalSpn").click(function(){
+          $("#deleteBookModal").hide();
+        });
+      }
+})
+
+// END OF DELETE
+
+// Search function
+
+function search(){
+
+    const test = document.getElementById("inputId").value;
+    const book1=test.toLowerCase();
+
+    var allBooks = JSON.parse(localStorage.getItem('books')) || [];
+    
+    
+    const book = allBooks.find(n=> n.title.toLowerCase()==book1);
+    if(book){
+        alert("YAY!! \n The book ' "+book.title+" ' you are searching is in our library.");
+    }
+    else{
+        alert("Error 404!!!\nHahaha Just kidding :) \nWe don't have this book.");
+    }
+}
 
 document.addEventListener('DOMContentLoaded',(event)=>{
 
@@ -380,5 +483,4 @@ $(document).ready(function () {
 
     });
 })
-
 
